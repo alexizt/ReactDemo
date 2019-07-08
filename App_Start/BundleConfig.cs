42f2fd923a8ec8f4e0322c3466f94f6ab246cbef
atriemstra <alexis.triemstra@corcoran.com>
2019-07-08 15:50:51 +0200

    first commit

---
+++ App_Start/BundleConfig.cs
@@ -0,0 +1,23 @@
+﻿using System.Web;
+using System.Web.Optimization;
+using System.Web.Optimization.React;
+
+namespace ReactDemo
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new BabelBundle("~/bundles/main").Include(
+                "~/Scripts/React/Tutorial.jsx"
+            ));
+
+            // Forces files to be combined and minified in debug mode
+            // Only used here to demonstrate how combination/minification works
+            // Normally you would use unminified versions in debug mode.
+            BundleTable.EnableOptimizations = true;
+
+        }
+    }
+}
